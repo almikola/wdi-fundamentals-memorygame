afd103e6dbd41832146506a4feca879e06973014
Aleksandra Mikolajczyk <almikola@Aleksandras-MacBook-Pro.local>
2016-11-19 15:49:21 +0000

    finished project - added images + mechanisms

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,5 +1,6 @@
 console.log("JS file is connected to HTML! Woo!")
 var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
 
 /*if (cardTwo === cardFour) {
 	alert('You found a match!');
@@ -19,12 +20,40 @@ else {alert('Sorry, try again.');
 }*/
 
 var gameBoard = document.getElementById('game-board');
-var createCards = function() {
+function createBoard() {
 for (var i = 0; i < cards.length; i++) {
 	var newCards = document.createElement('div');
 	newCards.className = 'card';
+	newCards.setAttribute('data-card', cards[i]);
+	newCards.addEventListener('click', isTwoCards);
+	
 	gameBoard.appendChild(newCards);
-};
-};
+	gameBoard.appendChild(newCards);
+}
+}
 
-createCards ();
+function isTwoCards() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<img src='king.png'>";
+	} else {
+		this.innerHTML = "<img src='queen.png'>";
+	}
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+		cardsInPlay = [];
+	}
+}
+
+
+function isMatch(cards) {
+	if (cards[0] === cards[1]) {
+		alert ("You've found a match!");
+	} else {
+		alert ("Sorry, try again.");
+	}
+}
+
+createBoard();
+
